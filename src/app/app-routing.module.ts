503aae6fc53a905bc209797ebffa12cb6f006eb4
narek.zakarian <denis.lubchenko@volo.global>
2019-11-22 17:02:36 +0200

    create header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin/admin.component';
+import { PublicComponent } from './public/public/public.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'admin', component: AdminComponent},
+    { path: 'public', component: PublicComponent},
+    { path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
