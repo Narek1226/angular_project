7cffcda83718ea382e8853cbb310013f05487e50
user <narek1226@gmail.com>
2020-01-05 15:17:44 +0200

    Create heroes app functionality

---
+++ src/app/pages/public/components/heroes-header/heroes-header.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeroesHeaderComponent } from './heroes-header.component';
+
+describe('HeroesHeaderComponent', () => {
+  let component: HeroesHeaderComponent;
+  let fixture: ComponentFixture<HeroesHeaderComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HeroesHeaderComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroesHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
